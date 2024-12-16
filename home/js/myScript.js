9b738f354634f220e0b2cc3d26203c0e34bd374b
Mariel <marielagravidez@gmail.com>
2024-12-16 16:19:11 +0800

    Update myScript.js

--- home/js/myScript.js
+++ home/js/myScript.js
@@ -1,3 +1,8 @@
-function myFunction() {
-    document.getElementById("demo").innerHTML = "Paragraph changed.";
+function button1() {
+    document.getElementById("paragraph").innerHTML = "Paragraph changed.";
+}
+    
+function button2() {
+    document.getElementById("color").style.color = "cornflowerblue";
+    document.getElementById("color").innerHTML = "Blue!"
 }
